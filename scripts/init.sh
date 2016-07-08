4c82114e85481d8cf71fec18f51d3c84c6c2b8f5
JeongJu Kim <haje01@gmail.com>
2016-07-08 16:45:58 +0900

    rename; fix env apply

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,5 +1,4 @@
-sudo yum install git
-
+sudo yum -y install git
 sudo pip install jupyter
 sudo pip install numpy
 sudo pip install matplotlib
--- scripts/run-jupyter.sh
+++ scripts/run-jupyter.sh
@@ -1,1 +1,1 @@
-IPYTHON_OPTS="notebook --no-browser --matplotlib=inline --ip=0.0.0.0 --port=8192" pyspark
+IPYTHON_OPTS="notebook --no-browser --matplotlib=inline --ip=0.0.0.0 --port=8192" pyspark --packages com.databricks:spark-csv_2.11:1.4.0
