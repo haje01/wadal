5dd57b6a6e909c07819249468bd2eb823750df9e
JeongJu Kim <haje01@gmail.com>
2016-07-12 18:06:57 +0900

    add ganglia

---
+++ bin/open_ganglia.sh
@@ -0,0 +1,1 @@
+open -a /Applications/Firefox.app http://$(aws emr describe-cluster --cluster-id `cat cluster_id.txt` --region=$AWS_REGION | grep MasterPublicDns | cut -d':' -f2 | tr -d ' ",' | awk '{print$1"/ganglia"}')
